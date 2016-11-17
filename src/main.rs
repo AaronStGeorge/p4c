5a345e00e3b78eeafb09c240cc197578de3fb916
Aaron St. George <aaronstgeorge@gmail.com>
2016-11-16 22:50:26 -0700

    Composite midpoint rule

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-/// Compute numerical integral of function of one real variable using trapezoidal rule.
+/// Compute numerical integral of function of one real variable using the
+/// composite trapezoidal rule.
 fn trapezoid(rhs: fn(f64) -> f64, a: f64, b: f64, n: i32) -> f64 {
     // Width of one trapezoid
     let h = (b - a) / n as f64;
-    h * (0.5 * rhs(a) + (1..n).fold(0.0, |acc, i| acc + rhs(a + i as f64 * h)) + 0.5 * rhs(b))
+    let inner_sum = (1..n).fold(0.0, |acc, i| acc + rhs(a + h * i as f64));
+    h * (0.5 * rhs(a) + inner_sum + 0.5 * rhs(b))
 }
 
-// right hand side function.
+/// Compute numerical integral of function of one real variable using the
+/// composite midpoint rule
+fn midpoint(rhs: fn(f64) -> f64, a: f64, b: f64, n: i32) -> f64 {
+    // Width of one rectangle
+    let h = (b - a) / n as f64;
+    h * (0..n).fold(0.0, |acc, i| acc + rhs(a + h * i as f64 + h / 2.0))
+}
+
+// 3t²e^(t^3).
 fn rhs(t: f64) -> f64 {
     3.0 * t.powi(2) * std::f64::consts::E.powf(t.powi(3))
 }
 
 fn main() {
-    println!("{}", trapezoid(rhs, 0.0, 1.0, 100));
+    println!("trapezoid: {}", trapezoid(rhs, 0.0, 1.0, 100));
+    println!("midpoint: {}", midpoint(rhs, 0.0, 1.0, 100));
 }
